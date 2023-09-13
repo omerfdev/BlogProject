a73b67a6e5bbec759d2421293a41132632b80bf5
omerfdev <omerfdev@gmail.com>
2023-09-13 19:22:33 +0300

    Add project files.

---
+++ Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Comment:IBase
+    {
+        public string Content { get; set; }
+        public int ID { get; set; }
+        public bool IsActive { get; set ; }
+        public DateTime CreateDate { get; set ; }
+        public DateTime UpdateDate { get; set; }
+        public DateTime DeleteDate { get; set; }
+    }
+}
---
+++ Domain/Enum/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enum
+{
+    public enum Status
+    {
+        Active,
+        Modified,
+        Passive
+
+    }
+}
